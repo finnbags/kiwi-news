02e9e4a8cd6ad093534f5938e5f7c05af2d394c5
Your Name <bagsintern@gmail.com>
2023-07-14 15:07:00 -0700

    resolve & go live

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppType } from "next/app";
 import { WagmiConfig, createConfig } from "wagmi";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { api } from "~/utils/api";
+import Head from 'next/head';
 
 import "~/styles/globals.css";
 
@@ -11,24 +12,47 @@ const config = createConfig(
     walletConnectProjectId: "3163f0f619168ae02436bfc44faa24a1",
     appName: "Kiwi News",
     appDescription: "The prime feed for hacker engineers building a decentralized future",
-    appUrl: "https://your-app-url.com", // Replace with your app's url
+    appUrl: "https://news.kiwistand.com/", // Replace with your app's url
     appIcon: "/favicon.ico", // Replace with your app's icon url, no bigger than 1024x1024px (max. 1MB)
   }),
 );
 
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-
-  
-    <WagmiConfig config={config}>
-      <ConnectKitProvider>
-        <Component {...pageProps} />
-      </ConnectKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        {/* Meta tags for SEO and social media */}
+        <meta name="description" content="My App" />
+        
+        {/* Open Graph */}
+        <meta property="og:title" content="My App" />
+        <meta property="og:description" content="The prime feed for hacker engineers building a decentralized future" />
+        <meta property="og:image" content="/kiwitag.png" />
+        <meta property="og:url" content="https://your-app-url.com" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kiwi News" />
+        <meta name="twitter:description" content="The prime feed for hacker engineers building a decentralized future" />
+        <meta name="twitter:image" content="/kiwitag.png" />
+        <meta name="twitter:creator" content="@kiwinews" />
+        
+        {/* Telegram */}
+        <meta property="telegram:site" content="kiwinews" />
+        
+        {/* Add other meta tags as needed */}
+      </Head>
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <Component {...pageProps} />
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </>
   );
 };
 
 export default api.withTRPC(MyApp);
+
 
 
